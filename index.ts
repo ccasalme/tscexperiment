406af7591306430968de3c6eea71a0a499b0dd9a
ccasalme <78170262+ccasalme@users.noreply.github.com>
2024-12-12 20:54:51 -0500

    added activity 1

---
+++ index.ts
@@ -0,0 +1,12 @@
+// This is an industrial-grade general-purpose greeter function:
+function greet(person, date) {
+    console.log(`Hello ${person}, today is ${date}!`);
+  }
+   
+  greet("Brendan");
+
+  //you should see the following output:
+  //Hello Brendan, today is undefined!
+  //This is because the date parameter is not being passed to the function.
+  //Expected 2 arguments, but got 1.
+  //To fix this, we need to pass the date parameter to the greet function.
